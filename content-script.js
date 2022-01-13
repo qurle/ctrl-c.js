78304c431ca38bdc405f03b14fa38887a367232a
nick <wrl.snk@gmail.com>
2022-01-13 16:46:36 +0300

    commit, innit?

---
+++ content-script.js
@@ -0,0 +1,26 @@
+// Selector of elements that will be copied
+const elementsToCopy = 'i'
+
+function handle(element) {
+    if (element.tagName.matches(elementsToCopy)) copy(element.innerHTML)
+}
+
+function copy(text) {
+    navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+        if (result.state == "granted" || result.state == "prompt") {
+            navigator.clipboard.writeText(text).then(function () {
+                console.log('copied\n' + element.innerHTML)
+            }, function () {
+                console.error('Extension Hotpot Icon to Clipboard failed to write to clipboard\nContact the dev at https://qurle.net/contact')
+            });
+        }
+    })
+
+}
+
+document.onclick = function (event) {
+    if (event === undefined) event = window.event
+    if (event.target instanceof Element)
+        handle(event.target)
+};
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# ctrl-c.js
+Initially made to copy SF Symbols from [Hotpot website](https://hotpot.ai/free-icons)
+
+![](https://github.com/qurle/ctrl-c.js/blob/main/cover.png?raw=true)
+
+This extension copies innerHTML of clicked element.  
+You can change manifest to add wished websites for extension to work.  
+If so, don't forget to change selector in `elementsToCopy`. Because we don't want to copy all elements, especially `body` content or smth.
+
+
